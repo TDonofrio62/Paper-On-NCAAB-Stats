73fd5ff0fbf1dc3599179badc09a0be73bde67b5
Default Jupyter user <thomasdonofrio31@gmail.com>
2022-04-21 19:04:11 -0400

    fixed datasets

--- Paper-On-NCAAB-Stats/scripts/02-data_cleaning.R
+++ Paper-On-NCAAB-Stats/scripts/02-data_cleaning.R
@@ -9,9 +9,14 @@
 # Use R Projects, not setwd().
 library(haven)
 library(tidyverse)
+
 # Read in the raw data. 
-raw_data <- readr::read_csv("inputs/data/raw_data.csv"
-                     )
+year1718 <- read_csv("inputs/data/2017-18 ncaab stats.csv")
+year1819 <- read_csv("inputs/data/2018-19 ncaab stats.csv")
+year1920 <- read_csv("inputs/data/2019-20 ncaab stats.csv")
+year2021 <- read_csv("inputs/data/2020-21 ncaab stats.csv")
+year2122 <- read_csv("inputs/data/2021-22 ncaab stats.csv")
+
 # Just keep some variables that may be of interest (change 
 # this depending on your interests)
 names(raw_data)
